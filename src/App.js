18015ccf84855d57e3440c4c4c77f50d634d56e8
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-03-21 18:29:11 +0900

    [feat] 로그인 기능 추가중

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import { React, useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
-  return <div className="App"></div>;
+  const currUser = authService.currentUser;
+  console.log(currUser);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(currUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Fwitter</footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/routes/Auth.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { authService } from "fbase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    name === "email" ? setEmail(value) : setPassword(value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (newAccount) {
+        // create account
+        const data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+        console.log(data);
+      } else {
+        // log in
+        const data = await signInWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+      <div>
+        <button>Countinue with Google</button>
+        <button>Countinue with Github</button>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
